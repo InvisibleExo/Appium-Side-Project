2c33b8327f9b28434feaba92c29446965edd6e77
Tony Lawrence <rigatoniman@hotmail.com>
2019-08-18 15:25:22 -0700

    Bash Start script added and update on ReadMe

---
+++ Start_TestSuite.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+plat=("$1")
+
+package=("$2")
+
+echo ""$plat" and "$package""
+
+# Passing parameters for -Dexec.args and setupArgs
+execArgs=("-Dplat=$plat" "-Dpackage=$package")
+
+setupArgs=("-Dplat=\"$plat\"" "-Dpackage=\"$package\"")
+
+# adding addtional parameters for setup and log capture
+while [ $3 ];
+do
+	val=("$3")
+	result=${val//[\"]/}
+	execArgs+=( "$result" )
+	setupArgs+=( "$3" )
+	shift
+done
+
+cleaning_input () {
+	local runcmd
+	declare runcmd="mvn exec:java -Dexec.mainClass=\"BaseTest.DriverTest\" -Dexec.args=\""${execArgs[@]}"\" "${setupArgs[@]}""
+	printf '%s\n' "${runcmd[*]}"
+	return $rundcmd
+}
+
+declare runTest=$(cleaning_input)
+echo $runTest
+eval $runTest
