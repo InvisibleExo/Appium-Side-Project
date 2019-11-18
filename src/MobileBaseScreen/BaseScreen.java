83833d18ae2e194a0bc1bf59bc62d913a42a405e
Tony Lawrence <rigatoniman@hotmail.com>
2019-11-17 23:09:48 -0800

    ReadME improvement and BaseScreen documentation

--- src/MobileBaseScreen/BaseScreen.java
+++ src/MobileBaseScreen/BaseScreen.java
@@ -13,6 +13,19 @@ import io.appium.java_client.MobileElement;
 
 import io.appium.java_client.pagefactory.AppiumFieldDecorator;
 
+/**
+* BaseScreen is a class to simulate the basic default functionality of mobile smart phone devices.
+* This class includes AppiumDriver class, a Wait class to support fluent and implicit waits, Swipe class, DeviceFunction class(for Go Back button, Home button,
+* and view Active tab,), Notification class(Open notification bar), and PageFactory class, (using AppiumFieldDecorator class for initElement() method parameter,) to support element initialization.
+* 
+* @Declaration Constructor parameters for new instance: BaseScreen(AppiumDriver<MobileElement> driver)
+* 
+* @Note BaseScreen currently supports Android initialization.
+* 
+* @author InvisibleExo
+* @Progress since Oct 2018
+
+*/
 public class BaseScreen {
 	protected AppiumDriver<MobileElement> driver;
 	protected AppiumDriverWait wait;
@@ -20,6 +33,12 @@ public class BaseScreen {
 	private DeviceFunction deviceCommands;
 	private NotificationBar notificationBar;
 
+/**
+* BaseScreen is a class to simulate the basic default functionality of mobile smart phone devices.
+* This class includes AppiumDriver class, a Wait class to support fluent and implicit waits, Swipe class, DeviceFunction class(for Go Back button, Home button,
+* and view Active tab,), Notification class(Open notification bar), and PageFactory class, (using AppiumFieldDecorator class for initElement() method parameter,) to support element initialization.
+ * @param driver
+ */
 	public BaseScreen(AppiumDriver<MobileElement> driver){
 		this.driver = driver;
 		determineSwipe(this.driver);
@@ -28,123 +47,190 @@ public class BaseScreen {
 		wait = new AppiumDriverWait(this.driver, 30);
 		PageFactory.initElements(new AppiumFieldDecorator(this.driver), this);
 	}
-	
+/**	
+ * This method is called once as part of the BaseScreen initialization. Based on the driver's platformName capability will determine if Swipe class is an
+ * instance of AndroidSwipe or IosSwipe.
+ * 
+ * @param AppiumDriver<MobileElement> driver
+*/	
 	void determineSwipe(AppiumDriver<MobileElement> driver) {
-		
-		if (driver.getCapabilities().getCapability("platformName").equals("Android")) {
-			swipe = new AndroidSwipe(driver);
+		switch(driver.getCapabilities().getCapability("platformName")+"") {
+			case "Android":
+				swipe = new AndroidSwipe(driver);
+				break;
+			case "IOS":
+				swipe = new IosSwipe(driver);
+				break;
 		}
-		else if (driver.getCapabilities().getCapability("platformName").equals("IOS")) {
-			swipe = new IosSwipe(driver);
-		}
-		
 	}
-	
+/**
+ *	This method is called once as part of the BaseScreen initialization. Based on the driver's platformName capability will determine if DeviceFunction class is 
+ * an instance of AndroidDeviceFunction or IosDeviceFucntion.
+ *
+ * @param AppiumDriver<MobileElement> driver
+ */
 	void determineDeviceFunction(AppiumDriver<MobileElement> driver) {
-		if (driver.getCapabilities().getCapability("platformName").equals("Android")) {
-			deviceCommands = new AndroidDeviceFunction(driver);
-		}
-		else if (driver.getCapabilities().getCapability("platformName").equals("IOS")) {
-			//Work on ios device funcitons
+		switch (driver.getCapabilities().getCapability("platformName")+""){
+			case "Android":
+				deviceCommands = new AndroidDeviceFunction(driver);
+				break;
+			case "IOS":
+				//Work on ios device funcitons
+				break;
 		}
 	}
-	
+/**
+ * This method is called once as part of the BaseScreen initialization. Based on the driver's platformName capability will determine if NotificationBar class is 
+ * an instance of AndroidNotificationBar or IosNotificationBar.
+ * 	
+ * @param AppiumDriver<MobileElement> driver
+ */
 	void determineDeviceNotifications(AppiumDriver<MobileElement> driver) {
-		if (driver.getCapabilities().getCapability("platformName").equals("Android")) {
-			notificationBar = new AndroidNotificationBar(driver);
-		}
-		else if (driver.getCapabilities().getCapability("platformName").equals("IOS")) {
-			//Work on ios device funcitons
+		switch (driver.getCapabilities().getCapability("platformName")+""){
+			case "Android":
+				notificationBar = new AndroidNotificationBar(driver);
+				break;	
+			case "IOS":
+				//Work on ios device funcitons
+				break;
 		}
 	}
-	
+/**
+ *  Sends a command to the NotificatonBar instance to open Notifications on device.	
+ */
 	public void openNotifications() {
 		notificationBar.openNotifications();
 	}
-	
+/**
+ * Sends a command to perform an upward swipe within the expanded notificationBar to close.	
+ */
 	public void closeNotifications() {
 		swipe.swipeThroughElementVertical(0.90, 0.05, 200, notificationBar.getNotificationScroller());
 	}
-	
+/**
+ * Sends a command to the DeviceFunction instance to perform the device's go back command.	
+ */
 	public void goBack() {
 		deviceCommands.goBack();
 	}
-	
+/**
+ * Sends a command to the DeviceFunction instance to take the device's home screen.
+ */
 	public void goHome() {
 		deviceCommands.goHome();
 	}
-	
+/**
+ * Sends a command to view the DeviceFunction instance to view any active apps from the device's home screen.	
+ */
 	public void viewActiveAppList() {
 		deviceCommands.viewActiveAppList();
 	}
-	
+/**
+ * Calls the device to view all active apps, search, and select the app based on the String input entered to resume on the app.	
+ * @param appName
+ */
 	public void resumeApp(String appName) {
 		deviceCommands.resumeApp(appName);
 	}
-
+/**
+ * Sends  start point and end point Coordinates, and duration instructions to Swipe instance to perform a vertical swipe along the device's screen. 
+ * The vertical swipe is perform within the middle of the screen.
+ * 
+ * @Note Shorter duration value means a faster swipe is performed.
+ * @param startPercentage
+ * @param finalPercentage
+ * @param duration
+ */
 	public  void  swipeVertical  (double startPercentage, double finalPercentage, int duration) {
 		swipe.swipeVertical(startPercentage, finalPercentage, duration);
 	} 
-	
+/**
+ * Sends  start point and end point Coordinates, and duration instructions to Swipe instance to perform a vertical swipe within the boundaries of the element provided in the parameters. 
+ * The vertical swipe is perform within the middle of the element provided in the method's parameters.
+ * 	
+ * @param startPercentage
+ * @param endPercentage
+ * @param duration
+ * @param element
+ */
 	public void swipeThroughElementVertical (double startPercentage, double endPercentage, int duration, MobileElement element) {
 		swipe.swipeThroughElementVertical(startPercentage, endPercentage, duration, element);
 	}
-	
+/**
+ * Sends  start point and end point Coordinates, and duration instructions to Swipe instance to perform a horizontal swipe along the device's screen. 
+ * The horizontal swipe is perform within the middle of the screen.
+ * 	
+ * @param startPercentage
+ * @param finalPercentage
+ * @param duration
+ */
 	public void swipeHorizontal (double startPercentage, double finalPercentage, int duration) {
 		swipe.swipeHorizontal(startPercentage, finalPercentage, duration);
 	}
-	
+/**
+ * Sends start point and end point Coordinates, and duration instructions to Swipe instance to perform a horizontal swipe within the boundaries of the element provided. 
+ * The horizontal swipe is perform within the middle of the element provided in the method's parameters.
+ * 	
+ * @param startPercentage
+ * @param endPercentage
+ * @param duration
+ * @param element
+ */
 	public void swipeThroughElementHorizontal (double startPercentage, double endPercentage, int duration, MobileElement element) {
 		swipe.swipeThroughElementHorizontal(startPercentage, endPercentage, duration, element);
 	}
-	
+/**
+ * Direction contain a list of enum variables to help the more complex swipe methods determine which direction the swipe will move towards. 
+ * List of enum variables are: (up and down). 
+ * 
+ * When selecting a Direction enum when required: (Example: Set the direction of the swipe to upward -> Direction.up) 
+ * 
+ * @author InvisibleExo
+ *
+ */
 	public enum Direction{
-		nw, sw, ne, se, up, down;
+		up, down;
 	}
-	public void swipeDiagonal (double startPercentageX, double startPercentageY, double finalPercentageX, 
-		double finalPercentageY, int duration, Direction dir) {
-		Swipe.Direction direct = null;
-		switch (dir) {
-		case nw:
-			direct = Swipe.Direction.nw;
-			break;
-		case sw:
-			direct = Swipe.Direction.sw;
-			break;
-		case ne:
-			direct = Swipe.Direction.ne;
-			break;
-		case se:
-			direct = Swipe.Direction.se;
-			break;
-		default:
-			break;
-		}
-		swipe.swipeDiagonalDirection(startPercentageX, startPercentageY, finalPercentageX, finalPercentageY, duration, direct);
+/**
+ * Sends a start and end points for both X and Y Axis. The first two parameters for this method at the start points, while third and forth parameters are the end points.
+ * ex: swipeCustomDirection(startPoint X, startPoint Y, endPoint X, endPoint Y, time duration). 
+ * To help determine where to set start and end points, the (0,0) point on the screen is the top left corner, while the bottom right corner is the max length and height point exp:(720, 1280).
+ * 
+ * @param startPercentageX
+ * @param startPercentageY
+ * @param finalPercentageX
+ * @param finalPercentageY
+ * @param duration
+ */
+	public void swipeCustomDirection (double startPercentageX, double startPercentageY, double finalPercentageX, 
+		double finalPercentageY, int duration) {
+		swipe.swipeCustomDirection(startPercentageX, startPercentageY, finalPercentageX, finalPercentageY, duration);
 	}
 	
-	public void swipeDiagonalThroughElement(double startPercentageX, double startPercentageY, double finalPercentageX, 
-			double finalPercentageY, int duration, Direction dir, MobileElement targetElement) {
-		Swipe.Direction direct = null;
-		switch (dir) {
-		case nw:
-			direct = Swipe.Direction.nw;
-			break;
-		case sw:
-			direct = Swipe.Direction.sw;
-			break;
-		case ne:
-			direct = Swipe.Direction.ne;
-			break;
-		case se:
-			direct = Swipe.Direction.se;
-			break;
-		default:
-			break;
-		}
-		swipe.swipeDiagonalDirectionThroughElement(startPercentageX, startPercentageY, finalPercentageX, finalPercentageY, duration, direct, targetElement);
+/**
+ * Similar to swipeCustomeDirection method, except range of the swipe within the selected target element pass into the method instead of the screen.
+ * 
+ * @param startPercentageX
+ * @param startPercentageY
+ * @param finalPercentageX
+ * @param finalPercentageY
+ * @param duration
+ * @param targetElement
+ */
+	public void swipeCustomDirectionThroughElement(double startPercentageX, double startPercentageY, double finalPercentageX, 
+			double finalPercentageY, int duration, MobileElement targetElement) {
+		swipe.swipeCustomDirectionThroughElement(startPercentageX, startPercentageY, finalPercentageX, finalPercentageY, duration, targetElement);
 	}
-	
+
+/**
+ * This method will continue to swipe either up or down(based on Direction enum provided,) until the desired MobileElement is found or the number of attempts provided for the method has hit its max.
+ * 
+ * @param method
+ * @param attempts
+ * @param targetElement
+ * @param dir
+ * @return MobileElement
+ */
 	public MobileElement swipeUntilFound(By method, int attempts, MobileElement targetElement, Direction dir) {
 		Swipe.Direction direct= null;
 		switch (dir) {
@@ -157,31 +243,58 @@ public class BaseScreen {
 		default:
 			break;
 		}
-		
 		return targetElement = swipe.swipeUntilFound(method, attempts, targetElement, direct);
 	}
-	
+/**
+ * Will continue checking for MobileElement by locator method until element 
+ * is now click-able.
+ * 	
+ * @param locatorName
+ */
 	public void checkIfElementisClickable(By locatorName) {
 		wait.waitUntilElementisClickable(locatorName);
 	}
-	
+/**
+ * Will continue checking for MobileElement by locator method until element 
+ * becomes present.
+ * 
+ * @param locatorName
+ */
 	public void checkIfElementisPresent(By locatorName) {
 		wait.waitUntilElementisPrensent(locatorName);
 	}
 	
+/**
+ * Returns a string of the device's current URL. This feature to be used when testing web-browser based applications.
+ * 
+ * @return String of Current URL
+ */
 	public String getCurrentUrl() {
 		return driver.getCurrentUrl();
 	}
-	
+/**
+ * Sends a command to web-browser to go to the provided http web address.
+ * The driver will wait to perform any further actions until the requested URL 
+ * has complete its loading in the browser.
+ * 	
+ * @param url
+ */
 	public void get(String url) {
 		driver.get(url);
 		wait.waitUntilPageLoaded();
 	}
 	
+/** 
+ * This method is to help create and provide a B64 Image of string for the parameters of 
+ * finElementByImage(b64Template). You must know the name of the image you have placed inside the ImageCheck directory 
+ * in order to retrieve the that image's b64 encoded String.
+ * 
+ * @param imageNameRequest
+ * @return String of Base64 Encoded scheme of image
+ * @throws IOException
+ */
 	public String getSpeicifiedImageB64(String imageNameRequest) throws IOException {
 		File refImgFile = new File("./ImageCheck/"+imageNameRequest);
 		return Base64.getEncoder().encodeToString(Files.readAllBytes(refImgFile.toPath()));
 	}
-	
-	
 }
